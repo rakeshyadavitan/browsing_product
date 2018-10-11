91974ea9c6a51583a733aa9cd35cde51ee5d58c5
Rakesh Yadav <rakeshkumar_y@hcl.com>
2018-10-11 12:55:09 +0530

    api token change

--- db/migrate/20181008120937_create_api_tokens.rb
+++ db/migrate/20181008120937_create_api_tokens.rb
@@ -1,9 +1,6 @@
 class CreateApiTokens < ActiveRecord::Migration
   def change
     create_table :api_tokens do |t|
-    	t.string  :device_id
-      t.integer :app_version
-      t.string  :os_platform
       t.string  :token
       t.string	:username
       t.timestamps null: false
